e740147c382f7ea38124b6c1cb37fdb71e8f351d
tomas.kindahl@outlook.com <tomas.kindahl@outlook.com>
2023-11-13 04:45:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace mju23h_dtp_genrep_FAS1
+{
+    internal class Program
+    {
+        static List<Todo> todolist;
+        class Todo
+        {
+        }
+        static void Main(string[] args)
+        {
+            // -4. Build a Todo class width a datetime and a description
+            // -3. Add a status to the class
+            // -2. Add a string method for the class
+            // -1. Add a print method for the todo item
+            //  1. Create a REPL with 'quit':
+            //  2. Create dummy todo items
+            //  3. Add 'list' that lists all active and waiting tasks
+            //  4. Add 'list all' that lists all tasks
+            //  5. Add 'new' that enables you to add new tasks
+            //  6. ... continue here ...
+        }
+    }
+}
